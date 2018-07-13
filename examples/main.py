3a839dbadbfd3cde3592c6e95b419745e7098aeb
Benjamin Ellenberger <benjamin@felfel.ch>
2018-07-13 10:41:18 +0200

    Extend the example to include usage of logging.Handlers together with our HttpSink

--- examples/main.py
+++ examples/main.py
@@ -2,20 +2,28 @@ from loggingpy.log import Logger
 import loggingpy.sink as sink
 import time
 import random
-from examples import uris
+import logging
+import sys
+from examples import uris  # you must provide these uri strings (just any uri that accepts requests.post(...) requests)
 
 if __name__ == "__main__":
-    # Sumologic token url
+    # Sumologic token url (just a basic string)
     sumoUri = uris.sumoUri
 
-    # Logz.io token url
+    # Logz.io token url (just a basic string)
     elasticUri = uris.elasticUri
 
+    # these are two sinks of type HttpSink, which extend the logging.Handler class
     sumoSink = sink.HttpSink(sumoUri)
     elasticSink = sink.HttpSink(elasticUri)
 
-    logger = Logger([sumoSink, elasticSink], "Calculator")
+    # however, you can use basic logging.Handler derived classes together with the ones here
+    stdoutSink = logging.StreamHandler(sys.stdout)
 
+    # the logger object takes the list of handlers and pushes the messages to them
+    logger = Logger([sumoSink, elasticSink, stdoutSink], "Calculator")
+
+    # this is just some basic code that generates different types of exceptions and then pushes different messages
     try:
         for i in range(0, 100000):
             try:

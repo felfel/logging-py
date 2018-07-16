ae1e14ac85f213a63a0ee7c82247f3a3f721fc0f
Benjamin Ellenberger <benjamin@felfel.ch>
2018-07-16 10:33:55 +0200

    Bump up version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='logging-py',
-      version='0.1.0',
+      version='0.1.2',
       url='https://github.com/felfel/logging-py',
       license='MIT License',
       author='felfel',

62e836da8f666286e190dfc1a4428eb04375d08c
fahyik <fahyik@felfel.ch>
2018-11-09 11:02:50 +0100

    Release 0.3.17

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='logging-py',
-      version='0.3.16',
+      version='0.3.17',
       url='https://github.com/felfel/logging-py',
       license='MIT License',
       author='felfel',
